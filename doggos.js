ac402c938de2fc80d7a8a6d114ac3cb6a01ceeb3
Sorrelljs <[sjsohota@gmail.com]>
2020-03-08 15:34:26 -0700

    added loader on ajax request

--- doggos.js
+++ doggos.js
@@ -2,16 +2,24 @@ const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
 const doggos = document.querySelector(".doggos");
 
+const loader = document.querySelector(".loader");
+
+
+$('.loader').hide();
+
 function addNewDoggo() {
     const promise = fetch(DOG_URL);
+    $('.loader').show();
     promise
         .then(function (response) {
             const processingPromise = response.json();
             return processingPromise;
         })
         .then(function (processedResponse) {
+            $('.loader').hide();
             const img = document.createElement("img");
             img.src = processedResponse.message;
+            img.className = "doggo-image"
             img.alt = "Cute doggo";
             doggos.appendChild(img);
         });
